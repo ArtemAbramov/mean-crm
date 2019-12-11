f4e151bc331bb54131f6ca1da75c8a407c732b31
Artem Abramov <tanukisuicide@yandex.ru>
2019-12-11 21:57:31 +0300

    Create auth service and made login logic on client side

--- client/src/app/login-page/login-page.component.ts
+++ client/src/app/login-page/login-page.component.ts
@@ -1,16 +1,24 @@
-import {Component, OnInit} from '@angular/core'
+import {Component, OnDestroy, OnInit} from '@angular/core'
 import {FormControl, FormGroup, Validators} from '@angular/forms'
+import {AuthService} from '../shared/services/auth.service'
+import {ActivatedRoute, Params, Router} from '@angular/router'
+import {Subscription} from 'rxjs'
 
 @Component({
   selector: 'app-login-page',
   templateUrl: './login-page.component.html',
   styleUrls: ['./login-page.component.scss']
 })
-export class LoginPageComponent implements OnInit {
+export class LoginPageComponent implements OnInit, OnDestroy {
 
   form: FormGroup
+  aSub: Subscription
 
-  constructor() {
+  constructor(
+    private auth: AuthService,
+    private router: Router,
+    private route: ActivatedRoute
+    ) {
   }
 
   ngOnInit() {
@@ -24,10 +32,33 @@ export class LoginPageComponent implements OnInit {
         Validators.minLength(6)
       ])
     })
+
+    this.route.queryParams.subscribe((params: Params) => {
+      if (params['registered']) {
+      //
+      } else if (params['accessDenied']) {
+      //
+      }
+    })
   }
 
   onSubmit() {
+    this.form.disable()
+    this.aSub = this.auth.login(this.form.value).subscribe(
+      () => {
+        this.router.navigate(['/overview'])
+      },
+      err => {
+        console.log(err)
+        this.form.enable()
+      }
+    )
+  }
 
+  ngOnDestroy(): void {
+    if (this.aSub) {
+      this.aSub.unsubscribe()
+    }
   }
 
 }
