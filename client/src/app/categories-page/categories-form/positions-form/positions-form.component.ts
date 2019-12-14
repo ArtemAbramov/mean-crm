4f7e08f03ffffddc92bbdae7bc2cca3a863a3279
Artem Abramov <tanukisuicide@yandex.ru>
2019-12-14 13:15:40 +0300

    Added categories UI and logic, start work with positions.

---
+++ client/src/app/categories-page/categories-form/positions-form/positions-form.component.ts
@@ -0,0 +1,26 @@
+import {Component, Input, OnInit} from '@angular/core'
+import {PositionsService} from '../../../shared/services/positions.service'
+import {Position} from '../../../shared/interfaces'
+
+@Component({
+  selector: 'app-positions-form',
+  templateUrl: './positions-form.component.html',
+  styleUrls: ['./positions-form.component.scss']
+})
+export class PositionsFormComponent implements OnInit {
+
+  @Input('categoryId') categoryId: string
+  positions: Position[] = []
+
+  constructor(
+    private positionService: PositionsService
+  ) {
+  }
+
+  ngOnInit() {
+    this.positionService.fetch(this.categoryId).subscribe(positions => {
+      this.positions = positions
+    })
+  }
+
+}
