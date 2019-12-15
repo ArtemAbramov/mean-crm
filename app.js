18e0d4c3c7dcc9669c7f5e220b3eaa7fd955ace8
Artem Abramov <tanukisuicide@yandex.ru>
2019-12-15 11:52:58 +0300

    Prepared project to deploy

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const passport = require('passport')
+const path = require('path')
 const cors = require('cors')
 const morgan = require('morgan')
 const authRoutes = require('./routes/auth')
@@ -34,6 +35,18 @@ app.use('/api/category', categoryRoutes)
 app.use('/api/position', positionRoutes)
 app.use('/api/order', orderRoutes)
 app.use('/api/analytics', analyticsRoutes)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/dist/client'))
+
+    app.get('*', (req, res) => {
+        res.sendFile(
+            path.resolve(
+                __dirname, 'client', 'dist', 'client', 'index.html'
+            )
+        )
+    })
+}
 
 
 module.exports = app
